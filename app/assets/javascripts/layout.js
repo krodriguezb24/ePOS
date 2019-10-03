f73725db551a6e6bd0e2f40f6037b28f26f7618d
Karla Rodríguez <k.rodriguezb@hotmail.com>
2019-10-03 13:33:24 -0500

    Animaciones en Menú

--- app/assets/javascripts/layout.js
+++ app/assets/javascripts/layout.js
@@ -2,12 +2,15 @@ window.onload = function(){
     window.frames['header__object'].contentDocument.getElementById('header_content__menu__icon').addEventListener("click", menuToggle)
     
     function menuToggle() {
-        console.log('hola!')
         window.frames['menu__object'].contentDocument.getElementById('menu_body');
-        if (window.frames['menu__object'].contentDocument.getElementById('menu_body').style.display === "none") {
-            window.frames['menu__object'].contentDocument.getElementById('menu_body').style.display = "flex";
-        } else {
-            window.frames['menu__object'].contentDocument.getElementById('menu_body').style.display = "none";
+        if (window.frames['menu__object'].contentDocument.getElementById('menu_body').classList.contains('show')) {
+            window.frames['menu__object'].contentDocument.getElementById('menu_body').classList.remove('show');
+            window.frames['menu__object'].contentDocument.getElementById('menu_body').classList.add('hide');
+            document.getElementById('menu').classList.add('hide')
+        } else if (window.frames['menu__object'].contentDocument.getElementById('menu_body').classList.contains('hide')) {
+            window.frames['menu__object'].contentDocument.getElementById('menu_body').classList.remove('hide');
+            window.frames['menu__object'].contentDocument.getElementById('menu_body').classList.add('show');
+            document.getElementById('menu').classList.remove('hide')
         }
     }
 }
